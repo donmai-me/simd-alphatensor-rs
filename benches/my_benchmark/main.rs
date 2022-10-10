13abf49a631d9992f89e2894dae48ed8b97fcfa5
donmai-me <71143298+donmai-me@users.noreply.github.com>
2022-10-10 21:45:35 +0800

    Added nalgebra and added blas to ndarray

--- benches/my_benchmark/main.rs
+++ benches/my_benchmark/main.rs
@@ -6,6 +6,8 @@ use simd_alphatensor_rs::{
 
 use ndarray::arr2;
 
+use nalgebra::{Matrix2, Matrix4};
+
 fn traditional_2x2x2(a: [i32; 4], b: [i32; 4]) {
     let a = arr2(&[[a[0], a[1]], [a[2], a[3]]]);
     let b = arr2(&[[b[0], b[1]], [b[2], b[3]]]);
@@ -28,6 +30,22 @@ fn traditional_4x4x4(a: [i32; 16], b: [i32; 16]) {
     a.dot(&b);
 }
 
+fn na_2x2x2(a: [i32; 4], b: [i32; 4]) {
+    // let a = Matrix2::new(a[0], a[1], a[2], a[3]);
+    // let b = Matrix2::new(b[0], b[1], b[2], b[3]);
+    let a = Matrix2::from_iterator(a);
+    let b = Matrix2::from_iterator(b);
+    a * b;
+}
+
+fn na_4x4x4(a: [i32; 16], b: [i32; 16]) {
+//    let a = Matrix4::new(a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8], a[9], a[10], a[11], a[12], a[13], a[14], a[15]);
+//    let b = Matrix4::new(b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7], b[8], b[9], b[10], b[11], b[12], b[13], b[14], b[15]);
+    let a = Matrix4::from_iterator(a);
+    let b = Matrix4::from_iterator(b);
+    a * b;
+}
+
 pub fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("simd_alphatensor_2x2x2", |b| {
         b.iter(|| {
@@ -37,9 +55,17 @@ pub fn criterion_benchmark(c: &mut Criterion) {
             )
         })
     });
-    c.bench_function("ndarry_2x2x2", |b| {
+    c.bench_function("ndarray_2x2x2", |b| {
         b.iter(|| {
             traditional_2x2x2(
+                black_box([1000, 2000, 3000, 4000]),
+                black_box([3000, 4000, 5000, 6000]),
+            )
+        })
+    });
+    c.bench_function("nalgebra_2x2x2", |b| {
+        b.iter(|| {
+            na_2x2x2(
                 black_box([1000, 2000, 3000, 4000]),
                 black_box([3000, 4000, 5000, 6000]),
             )
@@ -59,9 +85,23 @@ pub fn criterion_benchmark(c: &mut Criterion) {
             )
         })
     });
-    c.bench_function("ndarry_4x4x4", |b| {
+    c.bench_function("ndarray_4x4x4", |b| {
         b.iter(|| {
             traditional_4x4x4(
+                black_box([
+                    1000, 2000, 3000, 4000, 1000, 2000, 3000, 4000, 1000, 2000, 3000, 4000, 1000,
+                    2000, 3000, 4000,
+                ]),
+                black_box([
+                    1000, 2000, 3000, 4000, 1000, 2000, 3000, 4000, 1000, 2000, 3000, 4000, 1000,
+                    2000, 3000, 4000,
+                ]),
+            )
+        })
+    });
+    c.bench_function("nalgebra_4x4x4", |b| {
+        b.iter(|| {
+            na_4x4x4(
                 black_box([
                     1000, 2000, 3000, 4000, 1000, 2000, 3000, 4000, 1000, 2000, 3000, 4000, 1000,
                     2000, 3000, 4000,
